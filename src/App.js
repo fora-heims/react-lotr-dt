5fdbc5f65fd861666331c22497df91ec43154127
fora-heims <foraheims@gmail.com>
2021-12-13 13:27:02 -0800

    films data transformations are now set to the films prop. One background image is not displaying correctly due to an extra '-' where a space was added to the end of a movie title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 
 import CharacterList from './components/Characters/CharacterList';
 import FilmList from './components/Films/FilmList';
@@ -21,9 +21,16 @@ function App() {
       },
     });
     const data = await resp.json();
+    const fil = data.docs;
+    const filmData = fil.map((f) => {
+      return [
+        f.name,
+        f.name.toLowerCase().replace(/\s+/g, '-'),
+        f.boxOfficeRevenueInMillions,
+        f.academyAwardNominations,
+      ];
+    });
     console.log(data);
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/movie/ (don't forget to set your header!)
     // 2. Transform the response so that films contains nested arrays of:
     //   - the film's title
     //   - the film's title "slugified" i.e. in all lower case, with words separated with dashes,
@@ -33,13 +40,22 @@ function App() {
     // [["The Lord of the Rings Series", "the-lord-of-the-rings-series", 2917, 30 ], ["The Hobbit Series", "the-hobit-series", 2932, 7]...]
 
     // 3. Set the resulting transformation as state using setFilms
+    setFilms(filmData);
     // 4. You'll know it works if the films show up on the page
     return [];
   };
 
   const getCharacters = async () => {
-    // Add your code here!
-    // 1. Get data using fetch from https://the-one-api.dev/v2/character/
+    const resp = await fetch('https://the-one-api.dev/v2/character/', {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+      },
+    });
+    const data = await resp.json();
+    const char = data.docs;
+
+    console.log(data);
+
     // 2. Update the response data with the key `dates` which is a combination of
     //    the `birth` key and the `death key` separated with a dash. If neither date
     //    is provided, it should hold the string 'Unknown'
@@ -49,6 +65,7 @@ function App() {
     //       {name: 'Adrahil II', birth: "TA 2917", death: "TA 3010, dates: "TA 2917 - TA 3010", ...},
     //    ]
     // 3. Set the resulting transformation as state using setCharacters
+    setCharacters();
     // 4. You'll know it works if the characters show up on the page
     return [];
   };
@@ -64,7 +81,14 @@ function App() {
             Characters
           </NavLink>
         </header>
-        {/* ADD YOUR ROUTES HERE */}
+        <Switch>
+          <Route path="/films">
+            <FilmList films={films} />
+          </Route>
+          <Route path="/characters">
+            <CharacterList {...characters} />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
