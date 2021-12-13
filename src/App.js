3167f5e5478190c780ddaf5dd27ec98020285fbc
fora-heims <foraheims@gmail.com>
2021-12-13 13:45:03 -0800

    characters now set, with correct data transformation. Still need to fix bug in films page.

--- src/App.js
+++ src/App.js
@@ -25,24 +25,14 @@ function App() {
     const filmData = fil.map((f) => {
       return [
         f.name,
+        // FIX ME: the line below requires attention. one of the images is not shoeing up due to an excessive '-' at the end of a title
+        // NOTE: make sure you look at the response from the server - it may not be consistent
         f.name.toLowerCase().replace(/\s+/g, '-'),
         f.boxOfficeRevenueInMillions,
         f.academyAwardNominations,
       ];
     });
-    console.log(data);
-    // 2. Transform the response so that films contains nested arrays of:
-    //   - the film's title
-    //   - the film's title "slugified" i.e. in all lower case, with words separated with dashes,
-    //   - the box office total
-    //   - academy award nominations
-    // NOTE: make sure you look at the response from the server - it may not be consistent
-    // [["The Lord of the Rings Series", "the-lord-of-the-rings-series", 2917, 30 ], ["The Hobbit Series", "the-hobit-series", 2932, 7]...]
-
-    // 3. Set the resulting transformation as state using setFilms
     setFilms(filmData);
-    // 4. You'll know it works if the films show up on the page
-    return [];
   };
 
   const getCharacters = async () => {
@@ -53,21 +43,14 @@ function App() {
     });
     const data = await resp.json();
     const char = data.docs;
-
-    console.log(data);
-
-    // 2. Update the response data with the key `dates` which is a combination of
-    //    the `birth` key and the `death key` separated with a dash. If neither date
-    //    is provided, it should hold the string 'Unknown'
-    //    [
-    //       {name: 'Adanel', birth: "", death: "", dates: "Unknown", ...},
-    //       {name: 'Adrahil I', birth: "Before , TA 1944", death: "Late , Third Age", dates: "Before , TA 1944 - Late , Third Age", ...},
-    //       {name: 'Adrahil II', birth: "TA 2917", death: "TA 3010, dates: "TA 2917 - TA 3010", ...},
-    //    ]
-    // 3. Set the resulting transformation as state using setCharacters
-    setCharacters();
-    // 4. You'll know it works if the characters show up on the page
-    return [];
+    const charData = char.map((c) => {
+      if (c.birth !== '' && c.death !== '') {
+        return { ...c, dates: `${c.birth} - ${c.death}` };
+      } else {
+        return { ...c, dates: `Unknown` };
+      }
+    });
+    setCharacters(charData);
   };
 
   return (
@@ -86,7 +69,7 @@ function App() {
             <FilmList films={films} />
           </Route>
           <Route path="/characters">
-            <CharacterList {...characters} />
+            <CharacterList characters={characters} />
           </Route>
         </Switch>
       </BrowserRouter>
